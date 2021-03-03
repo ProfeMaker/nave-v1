1b1583b83b9d174502d2549550505ce9a2c2ec85
ProfeMaker <80003540+ProfeMaker@users.noreply.github.com>
2021-03-03 15:52:33 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,72 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+8 . . . . . . . . . . . . . . . 
+8 8 . . . . . . . . . . . . . . 
+. 8 8 8 8 8 8 8 8 8 8 . . . . . 
+. 5 8 8 8 8 8 8 8 8 8 8 . . . . 
+. 5 2 8 8 8 8 8 8 8 8 8 8 . . . 
+. 5 8 8 8 8 8 8 8 8 8 8 . . . . 
+. 8 8 8 8 8 8 8 8 8 8 . . . . . 
+8 8 . . . . . . . . . . . . . . 
+8 . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+5 . . . . . . . . . . . . . . . 
+. 2 1 1 1 1 1 1 1 1 1 1 1 . . . 
+5 5 3 3 3 3 3 3 3 3 3 3 . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":return img`
+. . . . . . . . . c c 8 . . . . 
+. . . . . . 8 c c c f 8 c c . . 
+. . . c c 8 8 f c a f f f c c . 
+. . c c c f f f c a a f f c c c 
+8 c c c f f f f c c a a c 8 c c 
+c c c b f f f 8 a c c a a a c c 
+c a a b b 8 a b c c c c c c c c 
+a f c a a b b a c c c c c f f c 
+a 8 f c a a c c a c a c f f f c 
+c a 8 a a c c c c a a f f f 8 a 
+. a c a a c f f a a b 8 f f c a 
+. . c c b a f f f a b b c c 6 c 
+. . . c b b a f f 6 6 a b 6 c . 
+. . . c c b b b 6 6 a c c c c . 
+. . . . c c a b b c c c . . . . 
+. . . . . c c c c c c . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,84 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    bala = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        5 . . . . . . . . . . . . . . . 
+        . 2 1 1 1 1 1 1 1 1 1 1 1 . . . 
+        5 5 3 3 3 3 3 3 3 3 3 3 . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, nave, 200, 0)
+    music.pewPew.play()
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    sprite.destroy()
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    info.changeLifeBy(-1)
+    music.baDing.play()
+    if (info.life() == 0) {
+        pause(2000)
+        music.setVolume(50)
+        music.smallCrash.play()
+    }
+})
+let meteoro: Sprite = null
+let bala: Sprite = null
+let nave: Sprite = null
+nave = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    8 . . . . . . . . . . . . . . . 
+    8 8 . . . . . . . . . . . . . . 
+    . 8 8 8 8 8 8 8 8 8 8 . . . . . 
+    . 5 8 8 8 8 8 8 8 8 8 8 . . . . 
+    . 5 2 8 8 8 8 8 8 8 8 8 8 . . . 
+    . 5 8 8 8 8 8 8 8 8 8 8 . . . . 
+    . 8 8 8 8 8 8 8 8 8 8 . . . . . 
+    8 8 . . . . . . . . . . . . . . 
+    8 . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+nave.setStayInScreen(true)
+controller.moveSprite(nave, 100, 100)
+info.setLife(3)
+info.setScore(0)
+music.setVolume(0)
+game.onUpdateInterval(1000, function () {
+    meteoro = sprites.create(img`
+        . . . . . . . . . c c 8 . . . . 
+        . . . . . . 8 c c c f 8 c c . . 
+        . . . c c 8 8 f c a f f f c c . 
+        . . c c c f f f c a a f f c c c 
+        8 c c c f f f f c c a a c 8 c c 
+        c c c b f f f 8 a c c a a a c c 
+        c a a b b 8 a b c c c c c c c c 
+        a f c a a b b a c c c c c f f c 
+        a 8 f c a a c c a c a c f f f c 
+        c a 8 a a c c c c a a f f f 8 a 
+        . a c a a c f f a a b 8 f f c a 
+        . . c c b a f f f a b b c c 6 c 
+        . . . c b b a f f 6 6 a b 6 c . 
+        . . . c c b b b 6 6 a c c c c . 
+        . . . . c c a b b c c c . . . . 
+        . . . . . c c c c c c . . . . . 
+        `, SpriteKind.Enemy)
+    meteoro.setVelocity(-80, 0)
+    meteoro.setPosition(180, randint(8, 112))
+})
